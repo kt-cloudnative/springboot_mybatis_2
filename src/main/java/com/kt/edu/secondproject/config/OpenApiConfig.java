09cac26db818625ef09d7b81ea0ccb1e96cade7b
LEE SEOK HWAN (이석환) <41466732+shclub@users.noreply.github.com>
2024-01-16 19:09:47 +0900

    Update OpenApiConfig.java

--- src/main/java/com/kt/edu/secondproject/config/OpenApiConfig.java
+++ src/main/java/com/kt/edu/secondproject/config/OpenApiConfig.java
@@ -1,6 +1,6 @@
 package com.kt.edu.secondproject.config;
 
-import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -57,4 +57,3 @@ public class OpenApiConfig {
     }
 
 }
-
